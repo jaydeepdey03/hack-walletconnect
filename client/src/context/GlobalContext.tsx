8e7ef4d34495103336fe953d70710adfcda5cc45
jaydeepdey03 <jaydeep.dey03@gmail.com>
2024-08-03 01:32:47 +0530

    added wagmi

--- client/src/context/GlobalContext.tsx
+++ client/src/context/GlobalContext.tsx
@@ -1,43 +1,56 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import {abi} from "@/lib/abi";
-import {useWeb3ModalProvider} from "@web3modal/ethers/react";
 import {Contract} from "ethers";
 import {BrowserProvider} from "ethers";
 import {createContext, ReactNode, useEffect, useState} from "react";
 
+import {createWeb3Modal} from "@web3modal/wagmi/react";
+import {defaultWagmiConfig} from "@web3modal/wagmi/react/config";
+
+import {WagmiProvider} from "wagmi";
+import {arbitrum, mainnet} from "wagmi/chains";
+import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
+
 export const GlobalContext = createContext({
-  contract: undefined as Contract | undefined,
+  ContractAddress: "",
 });
 
-const ContractAddress = "0x033Db7F7E7eF664Dd6dB84A0Eb77a0533013ff5A";
+const projectId = process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID!;
+
+const metadata = {
+  name: "Web3Modal",
+  description: "Web3Modal Example",
+  url: "https://web3modal.com", // origin must match your domain & subdomain
+  icons: ["https://avatars.githubusercontent.com/u/37784886"],
+};
+
+const chains = [mainnet, arbitrum] as const;
+
+const config = defaultWagmiConfig({
+  chains,
+  projectId,
+  metadata,
+});
+
+createWeb3Modal({
+  metadata,
+  wagmiConfig: config,
+  projectId,
+  enableAnalytics: true, // Optional - defaults to your Cloud configuration
+});
+
+const ContractAddress = "0x41B1Cbc18f5ffDDA64124FB55279561aaE2e53bd" as string;
 
 export function GlobalContextProvider({children}: {children: ReactNode}) {
-  const {walletProvider} = useWeb3ModalProvider();
-  const [contract, setContract] = useState<Contract>();
-
-  console.log(walletProvider, "walletProvider");
-
-  useEffect(() => {
-    (async function () {
-      try {
-        if (!walletProvider) return;
-        const provider = new BrowserProvider(walletProvider!);
-        const signer = await provider.getSigner();
-        const contract = new Contract(ContractAddress, abi, signer);
-        console.log(contract, "contract");
-        setContract(contract);
-      } catch (error: any) {
-        console.log(error.message);
-      }
-    })();
-  }, [walletProvider]);
   return (
-    <GlobalContext.Provider
-      value={{
-        contract,
-      }}
-    >
-      {children}
-    </GlobalContext.Provider>
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>
+        <GlobalContext.Provider value={{ContractAddress}}>
+          {children}
+        </GlobalContext.Provider>
+      </QueryClientProvider>
+    </WagmiProvider>
   );
 }
--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,45 +1,8 @@
 import "@/styles/globals.css";
 import type {AppProps} from "next/app";
-import {createWeb3Modal, defaultConfig} from "@web3modal/ethers/react";
 import {GlobalContextProvider} from "@/context/GlobalContext";
 
 export default function App({Component, pageProps}: AppProps) {
-  const projectId = process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID!;
-
-  const sepolia = {
-    chainId: 11155111,
-    name: "Sepolia",
-    currency: "ETH",
-    explorerUrl: "https://sepolia.etherscan.io",
-    rpcUrl: "https://ethereum-sepolia-rpc.publicnode.com",
-  };
-
-  // 3. Create a metadata object
-  const metadata = {
-    name: "lfgho-hackathon",
-    description: "AppKit Example",
-    url: "https://web3modal.com", // origin must match your domain & subdomain
-    icons: ["https://avatars.githubusercontent.com/u/37784886"],
-  };
-
-  const ethersConfig = defaultConfig({
-    /*Required*/
-    metadata,
-    auth: {
-      email: true, // default to true
-      socials: ["google", "x", "github", "discord", "apple"],
-      showWallets: true, // default to true
-      walletFeatures: true, // default to true
-    },
-  });
-
-  createWeb3Modal({
-    ethersConfig,
-    chains: [sepolia],
-    projectId,
-    enableAnalytics: true, // Optional - defaults to your Cloud configuration
-  });
-
   return (
     <GlobalContextProvider>
       <Component {...pageProps} />
